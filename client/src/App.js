2e9cf4f7c33e889f28f3693e775c5a5dc1b1683e
Jose Salazar <solisjose466@gmail.com>
2021-06-09 19:03:55 -0700

    set up routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,7 @@
 import React, { useState, useEffect } from "react";
+import Main from "./pages/Main.js";
 import Landing from "./pages/Landing.js";
 
 export default function App() {
-  const [form, setForm] = useState({ name: "", alt: "" });
-
-  function fetchForm(url) {
-    fetch(`/${url}`)
-      .then((response) => response.json())
-      .then((data) => setForm(data));
-  }
-
-  function submitCredentials(name, creds) {
-    console.log(creds);
-    fetch(`/${name}`, {
-      method: "POST",
-      body: JSON.stringify(creds),
-      headers: { "Content-type": "application/json; charset=UTF-8" },
-    })
-      .then((response) => response.json())
-      .then((json) => console.log(json));
-  }
-
-  useEffect(() => {
-    fetchForm("login");
-  }, []);
-
-  return (
-    <Landing
-      name={form.name}
-      alt={form.alt}
-      fetchForm={fetchForm}
-      submitCredentials={submitCredentials}
-    />
-  );
+  return <Landing />;
 }
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,56 +1,68 @@
 import React, { useState, useRef } from "react";
 
-export default function Landing({ name, alt, fetchForm, submitCredentials }) {
+export default function Landing() {
+  let login = {
+    name: "login",
+    alt: "register",
+  };
+
+  let register = {
+    name: "register",
+    alt: "login",
+  };
+  const [form, setForm] = useState(login);
+
+  function changeForm(obj) {
+    if (obj === login) {
+      setForm(register);
+    } else {
+      setForm(login);
+    }
+  }
+
+  function submitCredentials(creds) {
+    fetch(`/${form.name}`, {
+      method: "POST",
+      body: JSON.stringify(creds),
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }
   return (
     <>
       <div>
-        <strong>{name}</strong>
+        <strong>{form.name}</strong>
       </div>
       <SignInForm
-        name={name}
-        alt={alt}
-        fetchForm={fetchForm}
+        alt={form.alt}
         submitCredentials={submitCredentials}
+        changeForm={changeForm}
       />
     </>
   );
 }
 
-function SignInForm({ name, alt, fetchForm, submitCredentials }) {
+function SignInForm({ alt, submitCredentials, changeForm }) {
   const usernameRef = useRef("");
   const passwordRef = useRef("");
-  const [creds, setCreds] = useState({
+  let currentForm = {
     username: usernameRef.current.value,
     password: passwordRef.current.value,
-  });
-
-  function send() {
-    console.log({
-      username: usernameRef.current.value,
-      password: passwordRef.current.value,
-    });
-    setCreds({
-      username: usernameRef.current.value,
-      password: passwordRef.current.value,
-    });
-    submitCredentials(name, creds);
+  };
+  function returnCredentials() {
+    submitCredentials(currentForm);
   }
-
   return (
     <>
       <form>
-        <input placeholder="username" autoComplete="off" ref={usernameRef} />
-        <input
-          placeholder="password"
-          type="password"
-          autoComplete="off"
-          ref={passwordRef}
-        />
-        <button type="button" onClick={() => send()}>
+        <input placeholder="username" autoComplete="off" />
+        <input placeholder="password" type="password" autoComplete="off" />
+        <button type="button" onClick={returnCredentials()}>
           Submit
         </button>
       </form>
-      <button type="button" onClick={() => fetchForm(alt)}>
+      <button type="button" onClick={changeForm(currentForm)}>
         {alt} instead
       </button>
     </>
